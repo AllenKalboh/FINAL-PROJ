906f1b323f2793359c2d69be13c001e2831d4106
AllenKalboh <149352191+AllenKalboh@users.noreply.github.com>
2024-08-13 09:29:36 +0800

    loginformtemp

---
+++ login.php
@@ -0,0 +1,60 @@
+<?php
+include('db.php');
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql="SELECT*FROM users WHERE username='$username'";
+    $result=$conn->query($sql);
+
+    if($result->num_rows > 0){
+        $row=$result->fetch_assoc();
+        if(password_verify($password, $row['password'])){
+            header("Location: welcome.php");
+        }else{
+            echo "Invalid password!";
+        }
+    }else{ 
+        echo "No user found with that username!";
+    }
+    $conn->close();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+
+<!--  Login Form  -->
+
+<i class="fa-light fa-house"></i>
+
+<div class="container">
+        <h2>Login</h2>
+        <form action="" method="post">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            
+            <button type="submit" class="log-in-btn">Login</button>
+        </form>
+        <p>Don't have an account? </p>
+        
+        <button type="submit" class="reg-btn"> <a href="register.php"> Register </a> </button>
+    </div>
+
+
+
+
+</body>
+</html>
