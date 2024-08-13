9b661e6362b00c28084df5c0e8d3d41f86316915
AllenKalboh <149352191+AllenKalboh@users.noreply.github.com>
2024-08-13 18:18:31 +0800

    Laboratory Shet

--- register.php
+++ register.php
@@ -10,6 +10,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 
     if($conn->query($sql)===TRUE){
         echo "<script>alert('Registration Succesful');</script>";
+        header("Location: login.php");
     } else{
         echo "Error: " .$sql ."<br>". $conn->error;
     }
